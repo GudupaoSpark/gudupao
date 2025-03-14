db1c6660c4bb8776317ba8fab1e95354a2731d52
dongguacute <gudupao2022@gmail.com>
2025-03-14 23:46:30 +0800

    更新依赖问题

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 import { createApp } from 'vue'
+import { ParticlesPlugin } from "@tsparticles/vue3";
+import { loadFull } from "@tsparticles/engine";
 
-const app = createApp({})
+const app = createApp(App);
+
+app.use(ParticlesPlugin, {
+  init: async engine => {
+    await loadFull(engine);
+  }
+});
